c9c2590e4c5466b1037d53af12cb4cdfdba17855
Treer <treer.git+github@the-bordello.com>
2019-10-20 13:48:42 +1100

    update version info: v1.0.3 -> v1.0.4
    Also adds a solution file for VS2017 which drops the .Net Framework 2.0 version of the project

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2015 by Primoz Licen, Glenn Fisher")]
+[assembly: AssemblyCopyright("Copyright © 2015 — 2019 by Primoz Licen, Glenn Fisher")]
 [assembly: AssemblyDescription("Lists all the IPs")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
-[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.4")]
 [assembly: AssemblyProduct("ip4")]
 [assembly: AssemblyTitle("ip4")]
 [assembly: AssemblyTrademark("")]
